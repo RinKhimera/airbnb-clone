753894c83c40911d7c6436ec88504757ddd76af2
Samuel Pokam <dixiades@gmail.com>
2024-03-09 12:58:09 +0100

    uploadthings impl

--- app/actions.ts
+++ app/actions.ts
@@ -48,13 +48,32 @@ export const createCategoryPage = async (formData: FormData) => {
   return redirect(`/create/${homeId}/description`)
 }
 
-export const createDescription = (formData: FormData) => {
+export const createDescription = async (formData: FormData) => {
   const title = formData.get("title") as string
   const description = formData.get("description") as string
   const price = formData.get("price")
   const imageFile = formData.get("image") as File
+  const homeId = formData.get("homeId") as string
 
   const guestNumber = formData.get("guest") as string
   const roomNumber = formData.get("room") as string
   const bathroomNumber = formData.get("bathroom") as string
+
+  // const data = await prisma.home.update({
+  //   where: {
+  //     id: homeId,
+  //   },
+  //   data: {
+  //     title: title,
+  //     description: description,
+  //     price: Number(price),
+  //     bedrooms: roomNumber,
+  //     bathrooms: bathroomNumber,
+  //     guests: guestNumber,
+  //     photo: imageData?.path,
+  //     addedDescription: true,
+  //   },
+  // })
+
+  return redirect(`/create/${homeId}/address`)
 }
--- app/create/[id]/description/page.tsx
+++ app/create/[id]/description/page.tsx
@@ -1,3 +1,4 @@
+import { ButtonUploader } from "@/components/ButtonUploader"
 import { Counter } from "@/components/Counter"
 import CreationBottomBar from "@/components/CreationBottomBar"
 import { Card, CardHeader } from "@/components/ui/card"
@@ -5,7 +6,7 @@ import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { Textarea } from "@/components/ui/textarea"
 
-const DescriptionPage = () => {
+const DescriptionPage = ({ params }: { params: { id: string } }) => {
   return (
     <>
       <div className="mx-auto w-3/5">
@@ -15,75 +16,77 @@ const DescriptionPage = () => {
       </div>
 
       <form action="">
+        <input type="hidden" name="homeId" value={params.id} />
         <div className="mx-auto mb-36 mt-10 flex w-3/5 flex-col gap-y-5">
           <div className="flex flex-col gap-y-2">
             <Label>Title</Label>
             <Input
               name="title"
               type="text"
-              placeholder="Short and simple..."
               required
+              placeholder="Short and simple..."
+            />
+          </div>
+          <div className="flex flex-col gap-y-2">
+            <Label>Descrption</Label>
+            <Textarea
+              name="description"
+              required
+              placeholder="Please describe your home..."
             />
           </div>
 
           <div className="flex flex-col gap-y-2">
-            <Label>Description</Label>
-            <Textarea
-              name="decription"
-              placeholder="Please describe your home..."
+            <Label>Price</Label>
+            <Input
+              name="price"
+              type="number"
               required
+              placeholder="Price per Night in USD"
+              min={10}
             />
-
-            <div className="flex flex-col gap-y-2">
-              <Label>Price</Label>
-              <Input
-                name="price"
-                type="number"
-                placeholder="Price per night in USD"
-                min={10}
-                required
-              />
-            </div>
-
-            <div className="flex flex-col gap-y-2">
-              <Label>Image</Label>
-              <Input name="image" type="file" required />
-            </div>
-
-            <Card>
-              <CardHeader className="flex flex-col gap-y-5">
-                <div className="flex items-center justify-between">
-                  <div className="flex flex-col">
-                    <h3 className="font-medium underline">Guests</h3>
-                    <p className="text-sm text-muted-foreground">
-                      How many guests do you want?
-                    </p>
-                  </div>
-                  <Counter name="guest" />
-                </div>
-
-                <div className="flex items-center justify-between">
-                  <div className="flex flex-col">
-                    <h3 className="font-medium underline">Rooms</h3>
-                    <p className="text-sm text-muted-foreground">
-                      How many rooms do you have?
-                    </p>
-                  </div>
-                  <Counter name="room" />
-                </div>
-
-                <div className="flex items-center justify-between">
-                  <div className="flex flex-col">
-                    <h3 className="font-medium underline">Bathrooms</h3>
-                    <p className="text-sm text-muted-foreground">
-                      How many bathrooms do you have?
-                    </p>
-                  </div>
-                  <Counter name="bathroom" />
-                </div>
-              </CardHeader>
-            </Card>
           </div>
+
+          <div className="flex flex-col gap-y-2">
+            <Label>Image</Label>
+            <ButtonUploader />
+          </div>
+
+          <Input name="image" type="file" required />
+          <Card>
+            <CardHeader className="flex flex-col gap-y-5">
+              <div className="flex items-center justify-between">
+                <div className="flex flex-col">
+                  <h3 className="font-medium underline">Guests</h3>
+                  <p className="text-sm text-muted-foreground">
+                    How many guests do you want?
+                  </p>
+                </div>
+
+                <Counter name="guest" />
+              </div>
+              <div className="flex items-center justify-between">
+                <div className="flex flex-col">
+                  <h3 className="font-medium underline">Rooms</h3>
+                  <p className="text-sm text-muted-foreground">
+                    How many rooms do you have?
+                  </p>
+                </div>
+
+                <Counter name="room" />
+              </div>
+              <div className="flex items-center justify-between">
+                <div className="flex flex-col">
+                  <h3 className="font-medium underline">Bathrooms</h3>
+                  <p className="text-sm text-muted-foreground">
+                    How many bathrooms do you have?
+                  </p>
+                </div>
+
+                <Counter name="bathroom" />
+              </div>
+            </CardHeader>
+          </Card>
         </div>
 
         <CreationBottomBar />
