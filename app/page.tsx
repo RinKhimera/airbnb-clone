094317ba702b71353cff341a15ed87b688cfbe69
Samuel Pokam <dixiades@gmail.com>
2024-03-18 08:24:07 +0100

    adding new airbnb's

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 import ListingCard from "@/components/ListingCard"
 import MapFilterItems from "@/components/MapFilterItems"
+import NoItems from "@/components/NoItems"
+import SkeletonCard from "@/components/SkeletonCard"
 import prisma from "@/prisma/db"
-import Image from "next/image"
+import { Suspense } from "react"
 
 const getData = async ({
   searchParams,
@@ -39,7 +41,10 @@ export default function Home({
   return (
     <main className="container mx-auto px-5 lg:px-10">
       <MapFilterItems />
-      <ShowItems />
+
+      <Suspense key={searchParams?.filter} fallback={<SkeletonLoading />}>
+        <ShowItems searchParams={searchParams} />
+      </Suspense>
     </main>
   )
 }
@@ -54,16 +59,35 @@ const ShowItems = async ({
   const data = await getData({ searchParams: searchParams })
 
   return (
+    <>
+      {data.length === 0 ? (
+        <NoItems />
+      ) : (
+        <div className="mt-8 grid gap-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+          {data.map((item) => (
+            <ListingCard
+              key={item.id}
+              imagePath={item.photo}
+              description={item.description}
+              location={item.country}
+              price={item.price}
+            />
+          ))}
+        </div>
+      )}
+    </>
+  )
+}
+
+const SkeletonLoading = () => {
+  return (
     <div className="mt-8 grid gap-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-      {data.map((item) => (
-        <ListingCard
-          key={item.id}
-          imagePath={item.photo}
-          description={item.description}
-          location={item.country}
-          price={item.price}
-        />
-      ))}
+      <SkeletonCard />
+      <SkeletonCard />
+      <SkeletonCard />
+      <SkeletonCard />
+      <SkeletonCard />
+      <SkeletonCard />
     </div>
   )
 }
